cf5c6cb40c64c6bf660035ea3fb36b7534d76449
ijindal <fs6367@wayne.edu>
2023-08-08 10:38:41 -0700

    same code to read csv file

--- sample.py
+++ sample.py
@@ -1,9 +1,11 @@
+import os
 from ibm_watson_machine_learning.foundation_models.utils.enums import ModelTypes
 from ibm_watson_machine_learning.foundation_models import Model
 import json
-import os
+from data_util import DATA_DIR, csv2txt
 
 IBM_CLOUD_API_KEY = os.getenv("IBM_CLOUD_API_KEY")
+PROJECT_ID = "0beb9015-cd6d-467e-84a6-99f07254c022"
 
 print(json.dumps(ModelTypes._member_names_, indent=2))
 
@@ -15,7 +17,7 @@ my_credentials = {
 
 model_id = ModelTypes.MPT_7B_INSTRUCT2
 gen_parms = None
-project_id = "0beb9015-cd6d-467e-84a6-99f07254c022"
+project_id = PROJECT_ID
 space_id = None
 verify = False
 
@@ -33,4 +35,12 @@ gen_parms_override = None
 generated_response = model.generate(prompt_txt, gen_parms_override)
 
 print(json.dumps(generated_response, indent=2))
+
+
+with open(csv2txt("TB_demo_v2.csv"), "r") as f:
+    txt_data = f.readlines()
+
+print("text")
+
+
 
