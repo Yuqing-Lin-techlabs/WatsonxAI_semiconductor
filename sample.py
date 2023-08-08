d00f51c90476f8701ecd04aca23054fcc46e33a5
ijindal <fs6367@wayne.edu>
2023-08-07 17:14:03 -0700

    Add generation component

--- sample.py
+++ sample.py
@@ -4,8 +4,8 @@ import json
 import os
 
 IBM_CLOUD_API_KEY = os.getenv("IBM_CLOUD_API_KEY")
-print(IBM_CLOUD_API_KEY)
 
+print(json.dumps(ModelTypes._member_names_, indent=2))
 
 my_credentials = {
     "url": "https://us-south.ml.cloud.ibm.com",
@@ -23,4 +23,14 @@ model = Model(model_id, my_credentials, gen_parms, project_id, space_id, verify 
 
 model_details = model.get_details()
 
-print( json.dumps( model_details, indent=2 ) )
+print(json.dumps(model_details, indent=2))
+
+
+### Model output Generation
+prompt_txt = "In today's sales meeting, we "
+gen_parms_override = None
+
+generated_response = model.generate(prompt_txt, gen_parms_override)
+
+print(json.dumps(generated_response, indent=2))
+
